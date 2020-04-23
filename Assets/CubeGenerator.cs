500e8bc045d2d9eb740d037ca570b165c2a50be2
Ben Loesch <benloesch@gmail.com>
2020-04-22 19:28:52 -0700

    Third commit

--- Assets/CubeGenerator.cs
+++ Assets/CubeGenerator.cs
@@ -50,26 +50,21 @@ public class CubeGenerator : MonoBehaviour
         }
     }
 
-    private void ResetCubePosition()
-    {
-        for (int z = 0; z < cubeSize; z++)
-        {
-            for (int y = 0; y < cubeSize; y++)
-            {
-                for (int x = 0; x < cubeSize; x++)
-                {
-                    pieces[x,y,z].transform.localPosition = new Vector3(x, y, z);
-                }
-            }
-        }
-    }
 
-    void Rotate()
+    void Rotate(char face)
     {
-        //rotate rightRot
-        rightRot.transform.RotateAround(rightRot.transform.position, Vector3.right, 90f);
+        if(face == 'R')
+        {
+            //rotate rightRot
+            rightRot.transform.RotateAround(rightRot.transform.position, Vector3.right, 90f);
+        }else if(face == 'U')
+        {
+            //rotate topRot
+            topRot.transform.RotateAround(topRot.transform.position, Vector3.up, 90f);
+        }
+        
     }
-    void RotateFace2()
+    void RotateRFace()
     {
 
         for(int i = 6; i < transform.childCount; i++)
@@ -83,151 +78,51 @@ public class CubeGenerator : MonoBehaviour
             }
         }
 
-        Rotate();
-
-        Debug.Log("Children of RightRot: " + rightRot.transform.childCount);
-
-        /*for (int i = 6; i < kidCount-1; i++)
-        {
-            //this parents cubes to main cube
-            Transform temp = transform.GetChild(i);
-
-            if (temp.localPosition.x > 1.5f && temp.localPosition.x < 2.5f)    //right face
-            {
-                temp.transform.parent = transform; //parent to rightRot
-            }
-        }*/
+        Rotate('R');
 
         for(int i = 0; i < rightRot.transform.childCount; i++)
         {
-            Transform t = rightRot.transform.GetChild(i);
+            Transform t = rightRot.transform.GetChild(0);
             t.transform.parent = transform;
         }
     }
 
-    void RotAroundY()
+    void RotateUFace()
     {
-        int faceToRot = 2;
-        //GameObject newRotation = new GameObject();
-
-        //unparent cubes to be rotated
-        /*instead, have all the pivots be the first 6 children of gameobject
-         Then iterate through the rest of children and see if their localposition.X/Y/Z falls within the range of that face
-         Then parent them, rotate, unparent, etc*/
-        /*for(int x=0; x < cubeSize; x++)
+        for (int i = 6; i < transform.childCount; i++)
         {
-            for(int z=0; z < cubeSize; z++)
+            //this parents cubes to main cube
+            Transform temp = transform.GetChild(i);
+
+            if (temp.localPosition.y > 1.5f && temp.localPosition.y < 2.5f)    //top face
             {
-                pieces[x, faceToRot, z].transform.parent = topRot.transform;
+                temp.transform.parent = topRot.transform; //parent to rightRot
             }
-        }*/
+        }
 
-        //Rotate
-        //topRot.transform.Rotate(new Vector3(0f, 90f, 0f));
-        topRot.transform.RotateAround(topRot.transform.position,Vector3.up,90f);
+        Rotate('U');
 
-        //Parent back to main cube
-        /*for (int x = 0; x < cubeSize; x++)
+        for (int i = 0; i < topRot.transform.childCount; i++)
         {
-            for (int y = 0; y < cubeSize; y++)
-            {
-                for (int z = 0; z < cubeSize; z++)
-                {
-                    pieces[x, y, z].transform.parent = transform;
-                }
-            }
-        }*/
-        Debug.Log("unparented");
-
-        //Reset
-        //topRot.transform.Rotate(new Vector3(0f, -90f, 0f));
-        //topRot.transform.RotateAround(topRot.transform.position, Vector3.up, -90f);
-        //ResetCubePosition();
+            Transform t = topRot.transform.GetChild(0);
+            t.transform.parent = transform;
+        }
     }
 
-    void RotAroundX(int leftOrRight)
-    {
-        int faceToRot;
-        //GameObject newRotation = new GameObject();
-        if(leftOrRight == 1)
-        {
-            //rightRot.transform.position = new Vector3(2.75f, 1f, 1f);
-            faceToRot = 2;
-        }
-        else
-        {
-            //leftRot.transform.position = new Vector3(-2.75f, 1f, 1f);
-            faceToRot = 0;
-        }
-        
-
-        //unparent cubes to be rotated
-        for (int y = 0; y < cubeSize; y++)
-        {
-            for (int z = 0; z < cubeSize; z++)
-            {
-                if (leftOrRight == 1)
-                {
-                    pieces[faceToRot, y, z].transform.parent = rightRot.transform;
-
-                }
-                else
-                {
-                    pieces[faceToRot, y, z].transform.parent = leftRot.transform;
-
-                }
-            }
-        }
-
-        //Rotate
-        //rightRot.transform.Rotate(new Vector3(90f, 0f, 0f));
-        if(leftOrRight == 1) //right
-        {
-            rightRot.transform.RotateAround(rightRot.transform.position, Vector3.right, 90f);
-
-        }
-        else                //left
-        {
-            leftRot.transform.RotateAround(leftRot.transform.position, Vector3.left, 90f);
-
-        }
-
-        //Parent back to main cube
-        for (int x = 0; x < cubeSize; x++)
-        {
-            for (int y = 0; y < cubeSize; y++)
-            {
-                for (int z = 0; z < cubeSize; z++)
-                {
-                    pieces[x, y, z].transform.parent = transform;
-                }
-            }
-        }
-
-        //Rotate
-        //rightRot.transform.Rotate(new Vector3(-90f, 0f, 0f));
-        //rightRot.transform.RotateAround(rightRot.transform.position, Vector3.right, -90f);
-        ResetCubePosition();
-    }
+    
     void RotateFace()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            Debug.Log("Rotating Around Y...");
-            RotAroundY();
+            Debug.Log("Rotating top face");
+            RotateUFace();
         }
 
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Debug.Log("Rotating Around X...");
+            Debug.Log("Rotating right face");
             //RotAroundX(1);
-            RotateFace2();
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            Debug.Log("Rotating Around X...");
-            RotAroundX(0);
+            RotateRFace();
         }
 
     }
