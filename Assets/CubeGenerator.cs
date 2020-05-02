5119d2b600f1c8d6593df7ca8d743cb7f8d0bb58
Ben Loesch <benloesch@gmail.com>
2020-05-02 08:50:09 -0700

    I added UI buttons to control the FINALLY WORKING cube

--- Assets/CubeGenerator.cs
+++ Assets/CubeGenerator.cs
@@ -57,80 +57,146 @@ public class CubeGenerator : MonoBehaviour
         {
             //rotate rightRot
             rightRot.transform.RotateAround(rightRot.transform.position, Vector3.right, 90f);
-        }else if(face == 'U')
+        }
+        if(face == 'U')
         {
             //rotate topRot
             topRot.transform.RotateAround(topRot.transform.position, Vector3.up, 90f);
         }
+        if(face == 'F')
+        {
+            //rotate frontRot
+            frontRot.transform.RotateAround(frontRot.transform.position, Vector3.forward, 90f);
+        }
         
     }
-    void RotateRFace()
+    public void RotateRFace()
     {
+        List<Transform> temps = new List<Transform>();
 
-        for(int i = 6; i < transform.childCount; i++)
+        for (int i = 6; i < transform.childCount; i++)
         {
             //this parents cubes to main cube
             Transform temp = transform.GetChild(i);
 
-            if (temp.localPosition.x > 1.5f && temp.localPosition.x < 2.5f)    //right face
+            //Debug.Log("Searching for cubes...");
+            if (temp.localPosition.x > 1.5f && temp.localPosition.x < 2.5f)    //top face
             {
-                temp.transform.parent = rightRot.transform; //parent to rightRot
+                Debug.Log("Cube parented...");
+                temps.Add(temp);
+                //temp.transform.parent = topRot.transform; //parent to rightRot
             }
+        }
+        Debug.Log("Items in temps... " + temps.Count);
+
+        foreach (Transform t in temps)
+        {
+            t.transform.parent = rightRot.transform; //parent to toptRot
         }
 
         Rotate('R');
 
-        for(int i = 0; i < rightRot.transform.childCount; i++)
+        for (int i = 0; i < temps.Count; i++)
         {
             Transform t = rightRot.transform.GetChild(0);
             t.transform.parent = transform;
         }
     }
 
-    void RotateUFace()
+    public void RotateFFace()
     {
+        List<Transform> temps = new List<Transform>();
+
         for (int i = 6; i < transform.childCount; i++)
         {
             //this parents cubes to main cube
             Transform temp = transform.GetChild(i);
 
+            //Debug.Log("Searching for cubes...");
+            if (temp.localPosition.z > -.5f && temp.localPosition.z < .5f)    //front face
+            {
+                //Debug.Log("Cube parented...");
+                temps.Add(temp);
+                //temp.transform.parent = topRot.transform; //parent to rightRot
+            }
+        }
+
+        foreach (Transform t in temps)
+        {
+            t.transform.parent = frontRot.transform; //parent to frontRot
+        }
+
+        Rotate('F');
+
+        for (int i = 0; i < temps.Count; i++)
+        {
+            Transform t = frontRot.transform.GetChild(0);
+            t.transform.parent = transform;
+        }
+    }
+
+    public void RotateUFace()
+    {
+        List<Transform> temps = new List<Transform>();
+
+        for (int i = 6; i < transform.childCount; i++)
+        {
+            //this parents cubes to main cube
+            Transform temp = transform.GetChild(i);
+
+            //Debug.Log("Searching for cubes...");
             if (temp.localPosition.y > 1.5f && temp.localPosition.y < 2.5f)    //top face
             {
-                temp.transform.parent = topRot.transform; //parent to rightRot
+                Debug.Log("Cube parented...");
+                temps.Add(temp);
+                //temp.transform.parent = topRot.transform; //parent to rightRot
             }
+        }
+        Debug.Log("Items in temps... " + temps.Count);
+
+        foreach(Transform t in temps)
+        {
+            t.transform.parent = topRot.transform; //parent to toptRot
         }
 
         Rotate('U');
 
-        for (int i = 0; i < topRot.transform.childCount; i++)
+        for (int i = 0; i < temps.Count; i++)
         {
             Transform t = topRot.transform.GetChild(0);
             t.transform.parent = transform;
         }
     }
 
-    
-    void RotateFace()
-    {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Debug.Log("Rotating top face");
-            RotateUFace();
-        }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Debug.Log("Rotating right face");
-            //RotAroundX(1);
-            RotateRFace();
-        }
+    //void RotateFace()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.UpArrow))
+    //    {
+    //        Debug.Log("Rotating top face");
+    //        RotateUFace();
+    //    }
 
-    }
+    //    if (Input.GetKeyDown(KeyCode.RightArrow))
+    //    {
+    //        Debug.Log("Rotating right face");
+    //        //RotAroundX(1);
+    //        RotateRFace();
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.DownArrow))
+    //    {
+    //        Debug.Log("Rotating front face");
+    //        //RotAroundX(1);
+    //        RotateFFace();
+    //    }
+
+    //}
 
     // Update is called once per frame
     void Update()
     {
-        RotateFace();
+        //RotateFace();
         //Rotate();
     }
 }
